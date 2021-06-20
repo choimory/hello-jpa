cab90bce69e513ea24b962dcda3d4a229b0aadb2
choimory <choimory@naver.com>
2021-06-20 19:35:16 +0900

    JPA Auditing

---
+++ src/main/java/com/choimory/hello/hellojpa/common/entity/CommonDateTimeEntity.java
@@ -0,0 +1,22 @@
+package com.choimory.hello.hellojpa.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class CommonDateTimeEntity {
+    @CreatedDate
+    private LocalDateTime createDateTime;
+    @LastModifiedDate
+    private LocalDateTime updateDateTime;
+}
